3f0298cba01ffc6d079d879136cba91b8a5513bb
Petre Stegaroiu <2082041S@student.gla.ac.uk>
2016-10-16 17:56:16 +0100

    LDA tested. Added corpus of documents with different alpha vectors. Implemented multithreading.

--- LDA.py
+++ LDA.py
@@ -48,12 +48,27 @@ def generate_topic_distribution_of_words(random):
     return topic_word_distribution
 
 
-def generate_documents():
+def generate_corpus():
+    words_generated = 0
+    for c in range(corpus_size):
+        alpha_collection.append(numpy.random.uniform(low=0.0, high=1.0, size=5))
+        number_of_documents = numpy.random.randint(1, 100)  # documents
+        words_per_document = numpy.random.randint(1, 100)
+        words_generated += number_of_documents * words_per_document
+        print "corpus ", c , " has ", number_of_documents, " documents each containing ",\
+            words_per_document, " words with alpha equal to: ",alpha_collection[c]
+        dictionary_of_documents = {}
+        generate_documents(alpha_collection[c], number_of_documents, words_per_document, dictionary_of_documents)
+        corpus.append(dictionary_of_documents)
+    return words_generated
+
+
+def generate_documents(alpha, number_of_documents, words_per_document, dictionary_of_documents):
     # for each document
     for i in range(number_of_documents):
         document_dict = {}
-        # sample theta ~ Dir(alpha,alpha,alpha,alpha)
-        theta = numpy.random.dirichlet([alpha, alpha, alpha, alpha, alpha])
+        # sample theta ~ Dir(alpha)
+        theta = numpy.random.dirichlet(alpha)
         # error check
         if abs(sum(theta) - 1) > 0.01:
             print "theta not normalised"
@@ -73,7 +88,7 @@ def generate_documents():
         dictionary_of_documents[i] = document_dict
 
 
-def generate_gamma_and_phi_for_each_document():
+def generate_gamma_and_phi_for_each_document(dictionary_of_documents, document_parameters):
     for id in range(len(dictionary_of_documents)):
         gamma = numpy.random.uniform(low=0.0, high=1.0, size=5)
         # By reversing parameters of function we get wor_per_topic distribution
@@ -82,16 +97,13 @@ def generate_gamma_and_phi_for_each_document():
 
 
 def print_evaluation_criteria():
-    print "Comparing initial beta with LDA computed beta"
-    for i in range(number_of_topics):
-        print "Topic ", i, ":"
-        print initial_beta[i]
-        print beta[i]
     end_LDA = time.time()
-    print "Running the algorithm ", iterations, " times takes ", end_LDA - end_preprocess, " seconds"
+    LDA_time = end_LDA - end_preprocess
+    print "Running the algorithm ", iterations, " times takes ", LDA_time, " seconds"
+    print "Words per seconds = ", (iterations * number_of_words) / LDA_time
 
 
-def calculate_gamma():
+def calculate_gamma(alpha, document):
     for j in range(number_of_topics):
         topic_probability = 0
 
@@ -100,7 +112,7 @@ def calculate_gamma():
             word_count = document[word]
             topic_probability += word_probability * word_count
 
-        gamma[j] = alpha + topic_probability
+        gamma[j] = alpha[j] + topic_probability
 
 
 # Preprocessing
@@ -112,12 +124,11 @@ vocabulary = ["president", "vote", "corruption", "government",
               "actor", "movie", "TV", "cinema",
               "goal", "football", "goalkeeper", "penalty"]
 vocab_length = len(vocabulary)
-initial_beta = generate_topic_distribution_of_words(False)  # TODO change it to numpy array
-alpha = 0.1  # concentration of topics per document get ALPHA UPDATE
-number_of_documents = 100  # documents
-words_per_document = 50
-dictionary_of_documents = {}
-generate_documents()
+initial_beta = generate_topic_distribution_of_words(False)
+corpus_size = 3
+corpus=[]
+alpha_collection=[]
+number_of_words = generate_corpus()
 end_preprocess = time.time()
 print "Preprocessing takes ", end_preprocess - start, " seconds"
 
@@ -127,9 +138,12 @@ beta = generate_topic_distribution_of_words(True)  # topic X vocab_length
 word_in_vocab_dict = {}
 for index in range(vocab_length):
     word_in_vocab_dict[vocabulary[index]] = index;
-document_parameters = {}
-# for each document create document_per_topic_distribution and word_per topic distribution
-generate_gamma_and_phi_for_each_document()
+list_of_document_parameters = []
+for c in range(corpus_size):
+    document_parameters ={}
+    # for each document create document_per_topic_distribution and word_per topic distribution
+    generate_gamma_and_phi_for_each_document(corpus[c], document_parameters)
+    list_of_document_parameters.append(document_parameters)
 
 # Step 2 Run algorithm
 iterations = 100
@@ -137,36 +151,40 @@ epsilon = 0.01
 for it in range(iterations):
     # initialise new beta
     new_beta = numpy.full((number_of_topics,vocab_length),epsilon)
+    for c in range(corpus_size):
+        dictionary_of_documents = corpus[c]
+        document_parameters = list_of_document_parameters[c]
+        for n in range(len(dictionary_of_documents)):
+            gamma = document_parameters[n][0]  # document per topic distribution
+            document = dictionary_of_documents[n]
+            Ln = {}
+            phi = {}
+            # for each word recalculate topic probability Eq (6)
+            for word in document:
+                Ln[word] = numpy.zeros(number_of_topics)
+                phi[word] = numpy.zeros(number_of_topics)
+                for j in range(number_of_topics):
+                    Ln[word][j] = (numpy.log(beta[j][word_in_vocab_dict[word]]) + special.psi(gamma[j]) - special.psi(sum(gamma)))
+                B = - numpy.max(Ln[word])
+                exponential_Lnj_plus_B = []
+                for j in range(number_of_topics):
+                    exponential_Lnj_plus_B.append(numpy.exp(Ln[word][j] + B))
+                for j in range(number_of_topics):
+                    phi[word][j] = exponential_Lnj_plus_B[j] / numpy.sum(exponential_Lnj_plus_B)
+                    new_beta[j][word_in_vocab_dict[word]] += phi[word][j]
+            # for each document recalculate topic distribution Eq (7)
+            calculate_gamma(alpha_collection[c], document)
 
-    for n in range(number_of_documents):
-        gamma = document_parameters[n][0]  # document per topic distribution
-        document = dictionary_of_documents[n]
-        Ln = {}
-        phi = {}
-        # for each word recalculate topic probability Eq (6)
-        for word in document:
-            Ln[word] = numpy.zeros(number_of_topics)
-            phi[word] = numpy.zeros(number_of_topics)
-            for j in range(number_of_topics):
-                Ln[word][j] = (numpy.log(beta[j][word_in_vocab_dict[word]]) + special.psi(gamma[j]) - special.psi(sum(gamma)))
-            B = - numpy.max(Ln[word])
-            exponential_Lnj_plus_B = []
-            for j in range(number_of_topics):
-                exponential_Lnj_plus_B.append(numpy.exp(Ln[word][j] + B))
-            for j in range(number_of_topics):
-                phi[word][j] = exponential_Lnj_plus_B[j] / numpy.sum(exponential_Lnj_plus_B)
-                new_beta[j][word_in_vocab_dict[word]] += phi[word][j]
-        # for each document recalculate topic distribution Eq (7)
-        calculate_gamma()
+            # update document parameters
+            document_parameters[n][0] = gamma
+            document_parameters[n][1] = phi
 
-        # update document parameters
-        document_parameters[n][0] = gamma
-        document_parameters[n][1] = phi
-
-    # Normalise new computed beta and assign it to beta
-    for j in range(number_of_topics):
-        topic_sum = sum(new_beta[j])
-        for i in range(vocab_length):
-            beta[j][i] = new_beta[j][i] / topic_sum
-
+        beta_difference = 0
+        # Normalise new computed beta and assign it to beta
+        for j in range(number_of_topics):
+            topic_sum = sum(new_beta[j])
+            for i in range(vocab_length):
+                beta[j][i] = new_beta[j][i] / topic_sum
+                beta_difference += beta[j][i] - initial_beta[j][i]
+        #print numpy.abs(beta_difference)
 print_evaluation_criteria()
