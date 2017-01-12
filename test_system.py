1ffcd3ac6622e7c9e4005bafca4489d0d8252dbc
Petre Stegaroiu <2082041S@student.gla.ac.uk>
2017-01-12 09:32:19 +0000

    updated unit tests for vocabulary

--- test_system.py
+++ test_system.py
@@ -48,34 +48,47 @@ def document_has_right_format(document):
 
 class TestInputData(unittest.TestCase):
 
-    def test_vocabulary(self):
-        vocabulary = pickle.load(open("vocabulary.p","rb"))
-        for word in vocabulary:
+    def setUp(self):
+        self.vocabulary = pickle.load(open("vocabulary.p","rb"))
+        self.corpus_list = pickle.load(open("corpus.p", "rb"))
+    def test_vocabulary_word_format(self):
+        for word in self.vocabulary:
             is_right_format = word_has_right_format(word)
             if is_right_format:
                 self.failIf(is_right_format,"Vocabulary word does not have right format: "+ word)
 
-        print "Vocabulary tests passed"
-
+    def test_vocabulary_for_duplicity(self):
+        contains_duplicates = len(self.vocabulary) != len(set(self.vocabulary))   
+        self.assertFalse(contains_duplicates,"Vocabulary contains duplicates")
     def test_corpus(self):
-        corpus_list = pickle.load(open("corpus.p", "rb"))
-        for corpus_name in corpus_list:
-            corpus = corpus_list[corpus_name]
+        vocab = {}
+        for corpus_name in self.corpus_list:
+            words_added = 0
+            corpus = self.corpus_list[corpus_name]
             for document_name in corpus:
                 document = corpus[document_name]
                 is_right_format = document_has_right_format(document_name)
                 self.failIf(is_right_format, "Document name within corpus " +corpus_name +
                             " does not have right format: " + document_name)
                 for word in document:
+                    if word not in vocab:
+                        vocab[word] = True
+                        words_added +=1
                     is_right_format = word_has_right_format(word)
                     self.failIf(is_right_format, "Word within corpus " +corpus_name +
                                 " within document "+ document_name +" does not have right format: " + word)
                     intensity = document[word]
                     self.failIf(not isfloat(intensity), "Word " + word + " within corpus " +corpus_name +
                                 " within document "+ document_name +" does not have right intensity: " + str(intensity))
+            print words_added,
 
-        print "Corpus tests passed"
+
+class TestOutputData(unittest.TestCase):
+    def setUp(self):
+        self.file_list = pickle.load(open("/results/corpus.p","rb")) 
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestInputData)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestOutputData)
     unittest.TextTestRunner(verbosity=2).run(suite)
