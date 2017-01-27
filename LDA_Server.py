5ee8628f0d6e1bb00a76d9b339b27e7534d851c6
2082041S <petrestegaroiu@gmail.com>
2017-01-27 16:30:58 +0000

    More Debugging messages

--- LDA_Server.py
+++ LDA_Server.py
@@ -1,74 +1,78 @@
 import socket
-from multiprocessing import Queue
 from multiprocessing.managers import SyncManager
 import time
 import cPickle as pickle
 import os
-from functools import partial
-from Queue import Queue as _Queue
 import numpy as np
 import sys
-
 from lda import VariationalLDA
-from LDA_Object import ldaObject
-#from LDA_generate_corpus import get_corpus_list, get_alpha_list
-
-
-class Queue(_Queue):
-    """ A picklable queue. """
-
-    def __getstate__(self):
-        # Only pickle the state we care about
-        return (self.maxsize, self.queue, self.unfinished_tasks)
-
-    def __setstate__(self, state):
-        # Re-initialize the object, then overwrite the default state with
-        # our pickled state.
-        Queue.__init__(self)
-        self.maxsize = state[0]
-        self.queue = state[1]
-        self.unfinished_tasks = state[2]
-
-
-def get_q(q):
-    return q
+from multiprocessing import Manager
+import pdb
 
 
 class JobQueueManager(SyncManager):
     pass
 
-def make_server_manager(port, authkey):
-    job_q = Queue()
-    result_q = Queue()
+# class Beta_Holder():
+#     def __init__(self, beta=[]):
+#         self.beta = beta
 
-    JobQueueManager.register('get_job_q', callable=partial(get_q, job_q))
-    JobQueueManager.register('get_result_q', callable=partial(get_q, result_q))
+def make_server_manager(port, authkey):
+    job_q = Manager().Queue()
+    result_q = Manager().Queue()
+    JobQueueManager.register('get_job_q', callable=lambda: job_q)
+    JobQueueManager.register('get_result_q', callable=lambda: result_q)
 
     manager = JobQueueManager(address=(socket.gethostname(), port), authkey=authkey)
     manager.start()
-    print('Server hostname: %s' %socket.gethostname())
-    print('Server started at port %s' % port)
+    print'Server hostname: %s' % socket.gethostname()
+    print'Server started at port %s' % port
     return manager
 
-def load_corpus_list(directory_path):
-    return pickle.load(open("corpus.p", "rb"))
+
+def load_corpus_dict(directory_paths):
+    corpus_dict = {}
+    for directory in directory_paths:
+        file_list = os.listdir(directory)
+        #vocabulary = {}
+        for file_name in file_list:
+            if file_name.endswith(".dict"):
+                corpus = pickle.load(open(directory +"/" + file_name,"rb"))
+                corpus_dict[file_name[:-5]] = corpus
+                # for document in corpus:
+                #     for word in corpus[document]:
+                #         vocabulary[word] = True
+    return corpus_dict
 
 
 def construct_vocabulary(corpus_dict):
     vocabulary_dict = {}
+    intensity_list = []
     for corpus_name in corpus_dict:
+        max_intensity = 0
+        fragment_name = ""
         corpus = corpus_dict[corpus_name]
         for document in corpus:
             for word in corpus[document]:
                 vocabulary_dict[word] = True
+                intensity = corpus[document][word]
+                if intensity > max_intensity:
+                    max_intensity = intensity
+                    fragment = word
+        intensity_list.append([corpus_name, max_intensity, fragment])
+
+    intensity_list.sort()
+    for el in intensity_list:
+        print el
     return vocabulary_dict.keys()
 
 
 def reduce_vocabulary_if_needed(corpus_dict, vocabulary):
-    max_vocabulary_length = 10 ** 5
+    max_vocabulary_length = 50000
     document_appearance_min_threshold = 10
     init_length = len(vocabulary)
-    # if there are too many words in the vocabulary the system must remove words that appear infrequently in documents
+    # if there are too many words in the vocabulary then
+    # the system must remove words that appear infrequently in documents
     if len(vocabulary) > max_vocabulary_length:
         new_vocabulary_dict = {}
         for corpus_name in corpus_dict:
@@ -93,6 +97,7 @@ def reduce_vocabulary_if_needed(corpus_dict, vocabulary):
                     if word in new_vocabulary_dict:
                         new_doc[word] = corpus[document][word]
                 corpus[document] = new_doc
+        vocabulary = new_vocabulary_dict.keys()
         print "Reduced vocabulary size from ",init_length," to ", len(vocabulary)
     return corpus_dict,vocabulary
 
@@ -109,25 +114,31 @@ def send_names_to_slaves(shared_job_q, corpus_dict, number_of_topics, word_index
     for name in corpus_dict:
         corpus_name = name + "_LDA_result"
         corpus_names.append(corpus_name)
-        lda_object = VariationalLDA(corpus_dict[name], K=number_of_topics, eta=0.1, alpha=1, word_index=word_index)
+        lda_object = VariationalLDA(corpus_dict[name], K=number_of_topics, eta=0.1, alpha=1, word_index=word_index, normalise = 1000)
         shared_job_q.put([corpus_name, lda_object])
     return corpus_names
 
 
 def collect_betas_from_slaves(shared_result_q, shared_job_q, corpus_dict, number_of_topics, word_index,
                               corpus_names, beta_sum, new_beta, it, waiting_time_until_crash_assumed):
+
+    count = 1
     begin_iteration_time = time.time()
+    first_result_time = 0
     corpus_response_times = dict.fromkeys(corpus_names, 0)
     processor_names_received = []
     while set(corpus_names) != set(processor_names_received):
         try:
             result = shared_result_q.get_nowait()
             name = result[0]
-            # print "Received result", name
+            print "Received " + name + " " +str(count)+ "/"+ str(len(corpus_names))
             if name in processor_names_received:
                 shared_job_q.put_nowait(["beta", new_beta, it])
                 # print name
             else:
+                count +=1
+                if count == 1:
+                    first_result_time = time.time()
                 processor_names_received.append(name)
                 corpus_response_times[name] = time.time()
 
@@ -136,19 +147,24 @@ def collect_betas_from_slaves(shared_result_q, shared_job_q, corpus_dict, number
             # print processor_names_received
         except:
 
-            if it > 0 and (time.time() - begin_iteration_time > waiting_time_until_crash_assumed):
+            if first_result_time == 0:
+                client_crashed_assumed = (time.time() - begin_iteration_time) > waiting_time_until_crash_assumed
+            else:
+                client_crashed_assumed = (time.time() - first_result_time) > waiting_time_until_crash_assumed
+
+            if client_crashed_assumed:
+                pdb.set_trace()
                 begin_iteration_time = time.time()
                 waiting_time_until_crash_assumed += 60
                 for corpus_name in corpus_names:
                     if corpus_response_times[corpus_name] == 0:
                         name = corpus_name[:-11]
                         print "Client handling corpus ", corpus_name, " crashed", it
-                        lda_object = VariationalLDA(corpus_dict[name], number_of_topics, 0.1, 1,
-                                                    word_index=word_index)
-                        lda_object.beta = new_beta
-                        # shared_job_q.put(["crashed_" +corpus_name, lda_object, it-1])
+                        lda_object = VariationalLDA(corpus_dict[name], K=number_of_topics, eta=0.1, alpha=1,
+                                                    word_index=word_index, normalise = 1000)
+                        shared_job_q.put(["crashed_" +corpus_name, lda_object, it, new_beta])
                 time.sleep(10)
-                pass
+            pass
 
     return beta_sum
 
@@ -159,36 +175,55 @@ def normalize_beta(beta_sum):
 
 
 def send_new_betas_to_slaves(shared_job_q, corpus_dict, new_beta, it):
-    for j in range(len(corpus_dict)):
+    count = 0
+    for corpus_name in corpus_dict:
         result_not_put = True
+        # beta_holder = Beta_Holder(new_beta)
         while result_not_put:
             try:
-                shared_job_q.put_nowait(["beta", new_beta, it])
+                shared_job_q.put(["beta", new_beta, it])
                 result_not_put = False
+                count += 1
+                print "Sent", corpus_name, str(count)+"/"+ str(len(corpus_dict)), shared_job_q.qsize(),shared_job_q.full()
             except:
+                pdb.set_trace()
+                ex = sys.exc_info()[0]
                 result_not_put = True
 
 
 def signal_slaves_to_finish(shared_job_q, corpus_dict):
     for j in range(len(corpus_dict)):
-        shared_job_q.put(["Finished"])
+        result_not_put = True
+        while result_not_put:
+            try:
+                shared_job_q.put(["Finished"])
+                result_not_put = False
+            except:
+                result_not_put = True
+
+        
 
 
 def collect_output_files_from_slaves(shared_result_q, shared_job_q, corpus_names):
     processor_names_received = []
     files = {}
     while set(corpus_names) != set(processor_names_received):
-        result = shared_result_q.get()
+        result_not_gotten = True
+        while result_not_gotten:
+            try:
+                result = shared_result_q.get()
+                result_not_gotten = False
+            except:
+                result_not_gotten = True       
         name = result[0]
         if name in processor_names_received:
             shared_job_q.put(["Finished"])
             print name
         else:
             print name
-            print result[1]
             processor_names_received.append(name)
             # print processor_names_received
-            files[result[1]] = result[2]
+            files[name] = result[1]
     return files
 
 
@@ -206,8 +241,8 @@ def create_output_files(files):
 
 
 class Master():
-    def __init__(self, port, directory_path, file_name):
-        self.corpus_dict = load_corpus_list(directory_path)
+    def __init__(self, port, directory_paths):
+        self.corpus_dict = load_corpus_dict(directory_paths)
         print "Loaded input files"
         self.vocabulary = construct_vocabulary(self.corpus_dict)
         print "Constructed vocabulary of size", len(self.vocabulary)
@@ -224,13 +259,15 @@ class Master():
         corpus_names = send_names_to_slaves(self.shared_job_q,self.corpus_dict, self.number_of_topics, self.word_index)
         print corpus_names
 
-        waiting_time_until_crash_assumed = 300
+        waiting_time_until_crash_assumed = 360
         new_beta = np.zeros((self.number_of_topics, len(self.vocabulary)))
         it = 0
         convergence_number = 0.01
         beta_diff = self.number_of_topics
         while beta_diff > convergence_number:
-            beta_sum = np.zeros((self.number_of_topics, self.vocabulary_size))
+            iteration_start = time.time()
+            print "Starting iteration",it
+            beta_sum = np.zeros((self.number_of_topics, len(self.vocabulary)))
             beta_sum = collect_betas_from_slaves(self.shared_result_q, self.shared_job_q, self.corpus_dict,
                                                  self.number_of_topics, self.word_index, corpus_names,
                                                  beta_sum, new_beta, it, waiting_time_until_crash_assumed)
@@ -238,11 +275,11 @@ class Master():
             new_beta = normalize_beta(beta_sum)
             beta_diff = np.sum(abs(np.subtract(new_beta, old_beta)))
 
-
+            print "sending new_betas to workers"
             if beta_diff > convergence_number:
                 send_new_betas_to_slaves(self.shared_job_q, self.corpus_dict, new_beta, it)
 
-            print "iteration: ", it, "beta difference: ", beta_diff
+            print "iteration: ", it, "beta difference: ", beta_diff, "seconds taken:", time.time()- iteration_start
             it += 1
 
 
@@ -252,7 +289,6 @@ class Master():
         print "Got back all files " + str(len(files))
         create_output_files(files)
 
-
         end_time = time.time()
         print end_time - start_time, " seconds"
         time.sleep(5)
@@ -261,7 +297,11 @@ class Master():
 
 if __name__ == '__main__':
     port = 8765
-    if len(sys.argv) == 2:
-        port = int(sys.argv[1])
-    master = Master(port,"","")
+    directory_paths = ["/users/level4/2082041s/LDA/in/dicts",
+                    "/users/level4/2082041s/LDA/in/dicts_1",
+                    "/users/level4/2082041s/LDA/in/dicts_2",
+                    "/users/level4/2082041s/LDA/in/dicts_3"]
+    if len(sys.argv) > 1:
+        directory_paths = sys.argv[1:]
+    master = Master(port,directory_paths)
     master.runserver()
--- LDA_client.py
+++ LDA_client.py
@@ -1,42 +1,26 @@
-from multiprocessing import Queue
 import multiprocessing.managers
 import multiprocessing
 from multiprocessing.managers import SyncManager
 import time
-from Queue import Queue as _Queue
 import numpy as np
-
+from multiprocessing import Manager
 import sys
 
 
-class Queue(_Queue):
-    """ A picklable queue. """
 
-    def __getstate__(self):
-        # Only pickle the state we care about
-        return (self.maxsize, self.queue, self.unfinished_tasks)
-
-    def __setstate__(self, state):
-        # Re-initialize the object, then overwrite the default state with
-        # our pickled state.
-        Queue.__init__(self)
-        self.maxsize = state[0]
-        self.queue = state[1]
-        self.unfinished_tasks = state[2]
-
-
-def get_q(q):
-    return q
 
 class ServerQueueManager(SyncManager):
     pass
 
+# class Beta_Holder():
+#     def __init__(self, beta=[]):
+#         self.beta = beta
 
-def runclient(host,port):
+def runclient(host,port, number_of_cores):
     manager = make_client_manager(host, port, "test")
     job_q = manager.get_job_q()
     result_q = manager.get_result_q()
-    mp_work_allocator(job_q, result_q, multiprocessing.cpu_count())
+    mp_work_allocator(job_q, result_q, number_of_cores)
 
 
 def make_client_manager(host, port, authkey):
@@ -88,63 +72,98 @@ def LDA_worker(job_q, result_q):
                     initial_iteration = 0
                     corpus_received = True
                     corpus_name = job[0]
+                       
+                    lda_object = job[1]
+                    print process_name, "Received corpus", corpus_name  
+                    pre_LDA = time.time()
+                    lda_object.run_vb(verbose = False)
+                    print process_name, "finished running LDA on", corpus_name, "in", time.time() - pre_LDA," seconds"
                     if corpus_name.startswith("crashed_"):
                         corpus_name= corpus_name[8:]
                         server_iteration = job[2]
-                        current_iteration = server_iteration
                         initial_iteration = server_iteration
+                        lda_object.beta = job[3]                    
 
-                    print process_name, "Received corpus", corpus_name
-                    lda_object = job[1]
-                    lda_object.run_vb()
-                    corpus_list.append([corpus_name, lda_object, initial_iteration])
-                    current_lda_object_index = len(corpus_list) - 1                  
-                    result_q.put([corpus_name,lda_object.beta_matrix])
+                    
+                    corpus_list.append([corpus_name, lda_object, initial_iteration])    
+                    result_not_put = True
+                    # beta_holder = Beta_Holder(lda_object.beta_matrix)
+
+                    while result_not_put:
+                        try:
+                            result_q.put_nowait([corpus_name, lda_object.beta_matrix])
+                            result_not_put = False
+                        except:
+                            result_not_put = True          
                     print process_name, "sent back first beta of", corpus_name, initial_iteration
 
                 elif job[0].startswith("Finished"):
-                    current_lda_object_index  = (current_lda_object_index + 1) % len(corpus_list)
-                    current_corpus = corpus_list[current_lda_object_index]
-                    corpus_name = current_corpus[0]
-                    lda_object = current_corpus[1]
-                    filename = corpus_name
-                    result_not_put = True
-                    result_q.put([corpus_name, filename, lda_object.make_dictionary()])
-                    print corpus_name, " finished"                  
-                    corpus_count += 1
-                    if corpus_count >= len(corpus_list):
-                        return
+                    for current_corpus in corpus_list:
+                        corpus_name = current_corpus[0]
+                        lda_object = current_corpus[1]
+                        result_q.put([corpus_name, lda_object.make_dictionary()])
+                        print corpus_name, " finished"                  
+                    return
 
                 elif corpus_received and job[0].startswith("beta"):
+
+                    for corpus in range(len(corpus_list)-1):
+                            print corpus_list[corpus][0] + " EXTRA BETA"
+                            result_not_gotten = True
+                            while result_not_gotten:
+                                try:
+                                    job = job_q.get_nowait()
+                                    result_not_gotten = False
+                                except:
+                                    result_not_gotten = True
+                            
+                        
                     new_beta = job[1]
-                    current_lda_object_index  = (current_lda_object_index + 1) % len(corpus_list)
-                    current_corpus = corpus_list[current_lda_object_index]
-                    current_iteration = current_corpus[2]
-                    corpus_name = current_corpus[0]
-                    lda_object = current_corpus[1]
-                    lda_object.beta_matrix = new_beta
                     server_iteration = job[2]
-                    print corpus_name, current_iteration ,server_iteration
 
-                    # if processor is ahead of others then put back job and wait 3 seconds
-                    if current_iteration > server_iteration:
-                        too_fast_count +=1
-                        if too_fast_count > 1:
-                            print "Extra Beta floating due to client crash"
-                        else:
-                            job_q.put(job)                       
-                            time.sleep(5)
-                            print corpus_name," too fast"
-
-                    else:
-                        lda_object.run_vb(initialise=False)
-                        current_corpus[2] += 1
-                        result_not_put = True
-                        if np.isnan(lda_object.beta_matrix[0][0]):
-                            print "Corpus name",corpus_name
-                            print "First element of beta matrix", lda_object.beta_matrix[0][0]
-                        result_q.put([corpus_name, lda_object.beta_matrix])
+                    for corpus in corpus_list:
                         too_fast_count = 0
+                        corpus_name = corpus[0]
+                        lda_object = corpus[1]
+                        current_iteration = corpus[2]
+                        lda_object.beta_matrix = new_beta
+                        time.sleep(2)
+                        
+
+                        # if processor is ahead of others then put back job and wait 5 seconds
+                        if current_iteration > server_iteration:
+                            too_fast_count +=1
+                            if too_fast_count > 2:
+                                print "Extra Beta floating due to client crash"
+                            else:
+                                result_not_put = True
+                                while result_not_put:
+                                    try:
+                                        job = job_q.put(job) 
+                                        result_not_put = False
+                                    except:
+                                        result_not_put = True
+                                                      
+                                time.sleep(5)
+                                print corpus_name," too fast"
+                        else:
+                            pre_LDA = time.time()
+                            lda_object.run_vb(initialise=False, verbose = False)
+                            print process_name, "finished running LDA on", corpus_name, "in", time.time() - pre_LDA," seconds"
+                            # current_iteration += 1
+                            corpus[2] = current_iteration
+                            # beta_holder = Beta_Holder(lda_object.beta_matrix)
+                            # print "Created Beta_Holder"
+                            result_not_put = True
+                            while result_not_put:
+                                try:
+                                    result_q.put([corpus_name, lda_object.beta_matrix])
+                                    result_not_put = False
+                                    print corpus_name, current_iteration ,server_iteration
+                                    current_iteration += 1
+                                    corpus[2] = current_iteration
+                                except:
+                                    result_not_put = True   
 
                 else:
                     print "Error " + str(job[0])
@@ -155,14 +174,15 @@ def LDA_worker(job_q, result_q):
                     print process_name, "disconnected"
                     return
                 else:
-                    pass
+                    time.sleep(0.01)
 
     return
 
 if __name__ == '__main__':
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         host = sys.argv[1]
         port = int(sys.argv[2])
-        runclient(host, port)
+        number_of_cores = int(sys.argv[3])
+        runclient(host, port, number_of_cores)
     else:
-        print "Please provide <host> and <port>"
+        print "Please provide <host> and <port> and <number_of_cores>"
